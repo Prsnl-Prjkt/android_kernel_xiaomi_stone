dereference23/kernel_xiaomi_sm6375

techpack: display: Fix EDID for stock dtbo
Change-Id: Idf162af0e1cfce8b4021ae4f9b6279a22e1ce58f
Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

techpack/display/msm/dsi/dsi_drm.c
@@ -1014,6 +1014,13 @@ int dsi_connector_get_modes(struct drm_connector *connector, void *data,
 	edid.width_cm = (connector->display_info.width_mm) / 10;
 	edid.height_cm = (connector->display_info.height_mm) / 10;
 
+#ifdef CONFIG_WT_QGKI
+	if (connector->display_info.height_mm > 1000) {
+		edid.width_cm = (connector->display_info.width_mm) / 100;
+		edid.height_cm = (connector->display_info.height_mm) / 100;
+	}
+#endif
+
 	dsi_drm_update_dtd(&edid, modes, count);
 	dsi_drm_update_checksum(&edid);
 	rc =  drm_connector_update_edid_property(connector, &edid);

Prsnl-Prjkt/android_kernel_xiaomi_stone

Signed-off-by: RizkyMaulana58 <carlotta.montelli.0201@gmail.com>

techpack/display/msm/dsi/dsi_drm.c
@@ -1011,8 +1011,15 @@ int dsi_connector_get_modes(struct drm_connector *connector, void *data,
 		goto end;
 	}
 
+#ifdef CONFIG_HQ_QGKI
+	if (connector->display_info.height_mm > 1000) {
+		edid.width_cm = (connector->display_info.width_mm) / 100;
+		edid.height_cm = (connector->display_info.height_mm) / 100;
+	}
+#else
 	edid.width_cm = (connector->display_info.width_mm) / 10;
 	edid.height_cm = (connector->display_info.height_mm) / 10;
+#endif
 
 	dsi_drm_update_dtd(&edid, modes, count);
 	dsi_drm_update_checksum(&edid);